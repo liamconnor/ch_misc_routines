86dde337e13004afff32b6d4136115ecab5588de
Liam Connor <connor@gpc-f102n084.(none)>
2014-01-29 13:06:00 -0500

    updated output

--- pulsar/fold_pulsar_arr.py
+++ pulsar/fold_pulsar_arr.py
@@ -1,12 +1,20 @@
 import numpy as np
 import ch_pulsar_analysis as chp
 import h5py
+import misc_data_io as misc
+import glob
+
+DM = 26.833 # B0329 dispersion measure
+p1 = 0.7145817552986237 # B0329 period
+
+outfile = '/scratch/k/krs/connor/B0329_pulse_phase.hdf5'
+list = glob.glob('/scratch/k/krs/connor/chime/chime_data/20131210T060233Z/20131210T060233Z.h5.03*')
 
 #data_arr = abs(chd.data.data[:, 2, :].transpose())
 #time = (chd.fpga_count - chd.fpga_count[0]) * 0.010 / 3906.
 
-data_arr = 
-time = 
+data_arr, time, RA = misc.get_data(list[:10])[1:]
+data_arr = data_arr[:, 0, :]
 
 time_int = 500 # Integrate in time for 500 samples
 freq_int = 16 # Integrate over 16 freq bins
@@ -27,7 +35,7 @@ for freq in range(n_freq_bins):
                     start_chan=freq_int*freq, end_chan=freq_int*(freq+1), \
                     start_samp=time_int*tt, end_samp=time_int*(tt+1), f_ref=400.0)
 
-
+print "Writing folded array to", outfile
 f = h5py.File(outfile, 'w')
 f.create_dataset('folded_arr', data=folded_arr)
 f.close()
