de0ad6effe5a1c32e168fa5a2ea1fcda7aab6740
Liam Connor <connor@gpc-f103n084.(none)>
2014-02-11 17:15:03 -0500

    :quit

--- scripts/make_analysis_products.py
+++ scripts/make_analysis_products.py
@@ -1,17 +1,20 @@
 import numpy as np
 from scipy.stats import mode
 import h5py
+import argparse
 
 import misc_data_io as misc
 import lag_correct as lag_cor
 
+parser = argparse.ArgumentParser(description="This script locates the pulse bin, subtracts the offgates and calculates and corrects for lag")
+parser.add_argument("data_file", help="Folded data array with shape (nfreq, ncorr, ntimes, nbins)")
+parser.add_argument("data_outfile", help="")
+args = parser.parse_args()
+
 ncorr = 36
 
-#file = h5py.File('/scratch/k/krs/connor/B0329_10Dec2013.hdf5','r')
-#outfile = '/scratch/k/krs/connor/B0329_10Dec2013_analysisprod.hdf5'
-
-file = h5py.File('/scratch/k/krs/connor/B0329_9Feb2014.hdf5','r')
-outfile = '/scratch/k/krs/connor/B0329_9Feb2014_analysisprod.hdf5'
+file = h5py.File(args.data_file)
+outfile = args.data_outfile
 
 data_allcorr = file['folded_arr'][:]
 file.close()
@@ -33,7 +36,10 @@ PhAn = lag_cor.PhaseAnalysis(mode_on[0][0])
 lag_pixel = PhAn.get_lag_pixel(data_allcorr)
 lag_sol = PhAn.solve_lag(lag_pixel)
 
-print "delay:", np.round(lag_sol, 1)
+print lag_pixel
+print "Delay in pixels:", np.round(lag_sol, 1)
+print ""
+print "Delays in meters:", np.round(lag_sol * 0.85 * 3e8 / 400e6, 1)
 print ""
 
 data_zerolag = PhAn.correct_lag()[0]
