bddc429ecb0563fdce0cd3203f6b00914c59244d
liamconnor <liam.dean.connor@gmail.com>
2014-07-23 15:31:10 -0700

    changed input arguments

--- data_analysis/fit_transits_reader.py
+++ data_analysis/fit_transits_reader.py
@@ -13,14 +13,13 @@ from ch_util import data_index
 from datetime import datetime
 
 n_freq = 1024
-n_corr = 136
 
 parser = argparse.ArgumentParser(description="This programs tries to fit beam from point-source trans\
 its.")
 parser.add_argument("data_dir", help="Directory containing acquisition directories.")
-parser.add_argument("--src", help="Celestial objects to fit", default='All')
-parser.add_argument("--minfile", help="Minfile number e.g. 0051", default="")
-parser.add_argument("--maxfile", help="Maxfile number e.g. 0051", default="")
+parser.add_argument("--src", help="Source whose transit will be fit", default='CasA')
+parser.add_argument("--outdir", help="Directory output file gets written to", default="/home/liam/")
+parser.add_argument("--product_set", help="Which subset of correlations to fit (e.g. autos)", default=None)
 args = parser.parse_args()
 
 src = args.src
@@ -30,31 +29,45 @@ celestial_object = { "CasA": [344, 358, 58.83, ch_util.ephemeris.CasA], "TauA": 
 
 f = data_index.Finder()
 f.set_time_range(datetime(2014,03,20), datetime(2014,03,22))
-f.include_transits(celestial_object[src][3], time_delta=3600)
+f.include_transits(celestial_object[src][3], time_delta=5000)
 file_list = f.get_results()[0][0]
 
 for ii in range(len(file_list)):
     file_list[ii] = args.data_dir + file_list[ii]
 
+
+print ""
+print "Reading in Data:", file_list
+print ""
+
 dataobj = ch_util.andata.Reader(file_list)
 X = dataobj.read()
-vis, times = X.vis, X.timestamp
+vis, times, nprod = X.vis, X.timestamp, X.nprod
 RA = ch_util.ephemeris.transit_RA(times)
+nant = int((-1 + (1 + 4 * 2 * nprod)**0.5) / 2)
 
-print ""
-print "Reading in Data:", file
-print ""
+m = 3
+if args.product_set=='autos':
+    corrs = [misc.feed_map(i, i, nant) for i in range(nant)]
+elif args.product_set=='26m':
+    corrs = [misc.feed_map(i, m, nant) for i in range(nant)]
+else:
+    corrs = [0]
+
 print "RA range of data:", RA.min(),":",RA.max()
+print "for correlations", corrs
 
-beam_params = fm.beam_fit(vis, RA, dec = celestial_object[src][2])
+beam_params = fm.beam_fit(vis, RA, dec = celestial_object[src][2], correlations=corrs) # This is where the actual fit is done.
 
 transit_time = ch_util.ephemeris.datetime.fromtimestamp(times[0])
 date_str = transit_time.strftime('%Y_%m_%d')
-filename = '/scratch/k/krs/connor/beam_fit' + src + date_str + '.hdf5'
+filename = args.outdir + src + date_str + '.hdf5'
+
+print "Writing beam parameters file to %s" % filename
 
 g = h5py.File(filename,'w')
 g.create_dataset('beam', data = beam_params)
-g.create_dataset('visibilities', data = vis_obj)
-g.create_dataset('RA', data = RA_src)
+g.create_dataset('visibilities', data = vis)
+g.create_dataset('RA', data = RA)
 g.close()
 
