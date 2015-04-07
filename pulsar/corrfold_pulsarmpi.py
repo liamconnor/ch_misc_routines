9975c2c3328b6d35c567b3b291895b021c381d5e
Liam Connor <connor@gpc-f102n084-ib0.(none)>
2015-04-07 14:46:11 -0400

    Cleaned up corrfold

--- pulsar/corrfold_pulsarmpi.py
+++ pulsar/corrfold_pulsarmpi.py
@@ -10,7 +10,6 @@ import misc_data_io as misc
 import ch_util
 import ch_util.ephemeris as eph
 
-
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 print comm.rank, comm.size
@@ -43,7 +42,6 @@ dat_name = args.data_dir[-16:]
 list = glob.glob(args.data_dir + '/*h5*')
 list.sort()
 
-
 offs=0 # This is the file number offset, in case you only want to fold files n through m
 list = list[offs:offs + file_chunk * nnodes]
 
@@ -56,28 +54,26 @@ jj = comm.rank
 print "Starting chunk %i of %i" % (jj+1, nchunks)
 print "Getting", file_chunk*jj, ":", file_chunk*(jj+1)
 
-x = 0
-y = 1
+x = 1
+y = 0
 
-feeds = np.arange(nfeeds)
-nfeeds = len(feeds)
+feeds = np.arange(1, nfeeds)
 
 # The following chooses only correlations between feed i and the 26m feeds
-corrs = [misc.feed_map(i, x, nfeeds) for i in feeds] \
-              + [misc.feed_map(i, y, nfeeds) for i in feeds if i!=x] 
+corrs_x = [misc.feed_map(i, x, nfeeds) for i in feeds if i!=x] \
+#              + [misc.feed_map(i, y, nfeeds) for i in feeds if i!=x] 
 
-corrs.sort()
-#corrs_auto = [misc.feed_map(i, i, nfeeds) for i in feeds]
+corrs_auto = [misc.feed_map(i, i, nfeeds) for i in feeds]
 
 # Or you can just select random correlation products
-corrs = [0, 1, 16]
+corrs = corrs_x + corrs_auto
 
 ncorr = len(corrs)   
+corrs.sort()
     
 if jj == 0:
     print "RA, dec, DM, period:", RA_src, dec, DM, p1
     print "Using correlations", corrs
-
 
 data_reader_obj = ch_util.andata.Reader(list[file_chunk*jj:file_chunk*(jj+1)])
 data_reader_obj.prod_sel = corrs
@@ -85,6 +81,10 @@ data_reader_obj.prod_sel = corrs
 data_obj = data_reader_obj.read()
 data_arr = data_obj.vis
 time = data_obj.timestamp
+
+if jj == 0:
+    t0 = time[0]
+
 RA = eph.transit_RA(time)
 fpga_count = data_obj.index_map['time']['fpga_count']
 
@@ -96,20 +96,21 @@ freq_int = args.freq_int
 outdir = '/scratch/k/krs/connor/chime/calibration/' 
 
 if args.use_fpga==1:
+
     dt = 2048. / 800e6 #* 4.0 # 
     # THIS 4 IS ONLY FOR AUGUST 22ND WHOSE TIMESTAMPS ARE BAD
     time = (fpga_count) * dt 
+
     if jj==0:
         print "We're going with the fpga counts"
         print "Median del t", np.median(np.diff(time))
+
 
 n_freq_bins = np.round(data_arr.shape[0] / freq_int)
 n_time_bins = np.round(data_arr.shape[-1] / time_int)
 ngate = args.n_phase_bins
 
 RC = chp.RFI_Clean(data_arr, time)
-#RC.corrs = corrs
-#RC.frequency_clean()
 
 folded_arr, icount = RC.fold(DM, p1, ngate=ngate, ntrebin=time_int)
 
@@ -130,7 +131,7 @@ if os.path.isdir(outdir):
 else:
     os.mkdir(outdir)
 
-times_actually_full = comm.gather(time, root=0)
+times_actually_full = comm.gather(time, root=0) 
 
 for freq in range(n_freq_bins):
 
@@ -144,17 +145,21 @@ for freq in range(n_freq_bins):
 
         times = np.concatenate(times_actually_full)
 
-        outfile = outdir + args.pulsar + filename + np.str(freq_int) + np.str(args.add_tag) + np.str(freq) + '.hdf5'
+        outfile = outdir + args.pulsar + filename + \
+            np.str(freq_int) + np.str(args.add_tag) + np.str(freq) + '.hdf5'
 
         if os.path.isfile(outfile):
             os.remove(outfile)
 
         print "Writing output to", outfile
 
+        times += t0
+
         f = h5py.File(outfile, 'w')
         f.create_dataset('folded_arr', data=final_arr)
         f.create_dataset('icount', data=final_icount)
         f.create_dataset('times', data=times)
+        f.attrs.create('corrs', data=corrs)
         f.close()
 
 
